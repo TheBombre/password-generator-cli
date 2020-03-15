9ce1f7021d5e2d2b3c7b59855c585c250b910823
Carlton Ndhlovu <carltonndhlovu42@gmail.com>
2020-03-15 17:07:34 +0200

    Refactor and add argument to copy password to clipboard

--- main.py
+++ main.py
@@ -13,50 +13,58 @@ all_chars = {
 
 parser = argparse.ArgumentParser(description="A password generator cli")
 
-parser.add_argument("length", type=int,
+parser.add_argument("length",
+                    type=int,
                     help="the length of the password to be generated")
 
-parser.add_argument("-e", "--exclude-chars", help="characters you want to be omitted. l for lowercase, u for "
-                                                  "uppercase, d for digits, p for punctuation. Ensure this is the "
-                                                  "last argument you call", choices=['l', 'u', 'd', 'p'], nargs='+')
+parser.add_argument("-e", "--exclude-chars",
+                    help="characters you want to be omitted. l for lowercase, u for uppercase, d for digits, "
+                         "p for punctuation. Ensure that the length is specified before this command",
+                    choices=['l', 'u', 'd', 'p'],
+                    nargs='+')
 
-parser.add_argument("-v", "--verbosity", action="count", default=0,
-                    help="increase output verbosity")
+parser.add_argument('-c', "--copy",
+                    help="Copy the password to clipboard once generated",
+                    action="store_true")
 args = parser.parse_args()
 
 password = ''
 
+
+def concat_ascii_chars(chars_dict):
+    """
+    Join the remaining chars sets into one string
+
+    :param chars_dict: (dictionary) similar to all_chars variable
+    :return: string of the concatenation of all the values in chars_dict
+    """
+    joined_string = ''
+
+    for key in iter(chars_dict):
+        joined_string += chars_dict[key]
+
+    return joined_string
+
+
 if args.exclude_chars:
-    chars = ''
     # Remove the char sets not wanted by the user
     for charset in args.exclude_chars:
         del all_chars[charset]
 
-    # Join the remaining chars sets into one string
-    for key in iter(all_chars):
-        chars += all_chars[key]
+    chars = concat_ascii_chars(all_chars)
 
     chosen_chars = random.choices(list(chars), k=args.length)
     password = ''.join(chosen_chars)
 else:
-    chars = ''
-    # Join the remaining chars sets into one string
-    for key in iter(all_chars):
-        chars += all_chars[key]
+    chars = concat_ascii_chars(all_chars)
 
     chosen_chars = random.choices(list(chars), k=args.length)
     password = ''.join(chosen_chars)
 
-print(password)
-
-response = input("Do you want to copy the password to your clipboard? y (yes), n (no): \n")
-
-if response == 'y':
+if args.copy:
     pyperclip.copy(password)
 
-# if args.verbosity >= 2:
-#     print("the square of {} equals {}".format(2, 2))
-# elif args.verbosity >= 1:
-#     print("{}^2 == {}".format(2, 2))
-# else:
-#     print(2)
+print("password: ", password)
+
+
+
