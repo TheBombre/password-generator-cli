9690004e74b5a5823852b052cf4564d3d64c76cb
Carlton Ndhlovu <carltonndhlovu42@gmail.com>
2020-03-18 20:41:07 +0200

    Add functionality to randomise the characters chosen and begin work on verbose printing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import string
 
 import pyperclip
-
+# TODO: README.md
 all_chars = {
     'l': string.ascii_lowercase,
     'u': string.ascii_uppercase,
@@ -26,22 +26,51 @@ parser.add_argument("-e", "--exclude-chars",
 parser.add_argument('-c', "--copy",
                     help="Copy the password to clipboard once generated",
                     action="store_true")
+
+parser.add_argument('-v', "--verbose",
+                    help="print out what it going on",
+                    action="store_true")
 args = parser.parse_args()
 
 password = ''
 
 
+def verbos_printer(verbose, message):
+    # TODO: Add verbos printing
+    """
+    Prints out the message specified
+    :param message: text that will be printed to the console
+    :param verbose: boolean value of whether or not the user used the --verbose command
+    :return:
+    """
+    if verbose:
+        print(message)
+
+
+def shuffle_ascii_chars(ascii_chars):
+    """
+    Shuffles the chars around in the string
+    :param ascii_chars: string of ascii chars
+    :return: a string shuffled around
+    """
+    char_list = list(ascii_chars)
+    random.shuffle(char_list)
+    return ''.join(char_list)
+
+
 def concat_ascii_chars(chars_dict):
     """
     Join the remaining chars sets into one string
-
     :param chars_dict: (dictionary) similar to all_chars variable
     :return: string of the concatenation of all the values in chars_dict
     """
     joined_string = ''
 
     for key in iter(chars_dict):
-        joined_string += chars_dict[key]
+        shuffled_chars = shuffle_ascii_chars(chars_dict[key])
+        joined_string += shuffled_chars
+
+    shuffle_ascii_chars(joined_string)
 
     return joined_string
 
@@ -50,7 +79,7 @@ if args.exclude_chars:
     # Remove the char sets not wanted by the user
     for charset in args.exclude_chars:
         del all_chars[charset]
-
+    # TODO: Refactor this code as its repeated
     chars = concat_ascii_chars(all_chars)
 
     chosen_chars = random.choices(list(chars), k=args.length)
