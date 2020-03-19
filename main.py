2317a370484aa102c7f80cafed800e266cfd8702
Carlton Ndhlovu <carltonndhlovu42@gmail.com>
2020-03-19 18:39:35 +0200

    Add verbose functionality and add password generating function

--- main.py
+++ main.py
@@ -35,15 +35,14 @@ args = parser.parse_args()
 password = ''
 
 
-def verbos_printer(verbose, message):
-    # TODO: Add verbos printing
+def verbose_printer(verbose_val, message):
     """
     Prints out the message specified
     :param message: text that will be printed to the console
-    :param verbose: boolean value of whether or not the user used the --verbose command
+    :param verbose_val: boolean value of whether or not the user used the --verbose command
     :return:
     """
-    if verbose:
+    if verbose_val:
         print(message)
 
 
@@ -58,39 +57,62 @@ def shuffle_ascii_chars(ascii_chars):
     return ''.join(char_list)
 
 
-def concat_ascii_chars(chars_dict):
+def concat_ascii_chars(chars_dict, verbose_val):
     """
     Join the remaining chars sets into one string
+    :param verbose_val: boolean value of whether or not the user used the --verbose command
     :param chars_dict: (dictionary) similar to all_chars variable
     :return: string of the concatenation of all the values in chars_dict
     """
+    verbose_printer(verbose_val=verbose_val, message="Starting to join all characters into a single value")
+
     joined_string = ''
 
     for key in iter(chars_dict):
         shuffled_chars = shuffle_ascii_chars(chars_dict[key])
         joined_string += shuffled_chars
 
+    verbose_printer(verbose_val=verbose_val, message="Randomising all characters in the single value")
+
     shuffle_ascii_chars(joined_string)
 
     return joined_string
 
 
+def generate_password(char_dict, length, verbose_val):
+    """
+    Generates the password chooses random chars
+    :param verbose_val: boolean value of whether or not the user used the --verbose command
+    :param length: length of the password required
+    :param char_dict: dictionary of all the chars wanted which would be all_chars
+    :return: a password
+    """
+    verbose_printer(verbose_val=verbose_val, message="Generating password")
+
+    chars = concat_ascii_chars(chars_dict=char_dict, verbose_val=verbose_val)
+    chosen_chars = random.choices(list(chars), k=length)
+    return ''.join(chosen_chars)
+
+
+verbose = args.verbose
+
+if verbose:
+    print("Verbose turned on")
+
 if args.exclude_chars:
     # Remove the char sets not wanted by the user
+    verbose_printer(verbose_val=verbose, message="Removing unwanted characters")
     for charset in args.exclude_chars:
         del all_chars[charset]
-    # TODO: Refactor this code as its repeated
-    chars = concat_ascii_chars(all_chars)
 
-    chosen_chars = random.choices(list(chars), k=args.length)
-    password = ''.join(chosen_chars)
+    password = generate_password(char_dict=all_chars, length=args.length, verbose_val=verbose)
+
 else:
-    chars = concat_ascii_chars(all_chars)
+    password = generate_password(char_dict=all_chars, length=args.length, verbose_val=verbose)
 
-    chosen_chars = random.choices(list(chars), k=args.length)
-    password = ''.join(chosen_chars)
 
 if args.copy:
+    verbose_printer(verbose_val=verbose, message="Copying password to clipboard")
     pyperclip.copy(password)
 
 print("password: ", password)
